906a602c06159bc43d7442757fe296100daa5a4f
smsaidurrahmanrobin <smsaiyeed@gmail.com>
2022-03-14 20:15:01 +0600

    End of Section 16_Laravel
    polymorphic relation CRUD

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\Staff;
+use App\Models\Photo;
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +18,71 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/create', function (){
+
+    $staff = Staff::find(1);
+
+    $staff->photos()->create(['path'=>'another.jpg']);
+
+});
+
+Route::get('/read', function (){
+
+    $staff = Staff::findorfail(1);
+
+    foreach ($staff->photos as $photo){
+
+
+        return $photo->find(1)->path;
+
+    }
+
+});
+
+
+Route::get('/update', function (){
+
+    $staff = Staff::findorfail(1);
+
+    $photo = $staff->photos()->whereId(1)->first();
+
+    $photo->path = "Updated.jpg";
+    $photo->update();
+
+
+
+});
+
+Route::get('/delete', function (){
+
+    $staff = Staff::findorfail(1);
+
+   $staff->photos()->wherePath('Updated.jpg')->delete();
+
+
+
+});
+
+
+//-----------------extra techniques-----------------------
+
+
+Route::get('/assign', function (){
+
+    $staff = Staff::findorfail(1);
+
+    $photo = Photo::findorfail(5);
+
+    $staff->photos()->save($photo);
+
+});
+
+Route::get('/unassign', function (){
+
+    $staff = Staff::findorfail(1);
+
+    $staff->photos()->whereId(5)->update(['imageable_id'=>'0', 'imageable_type'=>'']);
+
 });
